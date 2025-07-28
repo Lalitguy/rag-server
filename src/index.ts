c075c4670b2ad82fe48beac3367a3890c980cdbb
Lalitguy <121784152+Lalitguy@users.noreply.github.com>
2025-07-29 01:29:55 +0530

    Added host as localhost for server serve

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { createServer } from "./config/fastify";
 const app = createServer();
 
 app.ready(() => {
-  app.listen({ port: Number(ENV.PORT) }, (err, address) => {
+  app.listen({ port: Number(ENV.PORT), host: "0.0.0.0" }, (err, address) => {
     if (err) {
       app.log.error(err);
       process.exit(1);
